f4f169bbd2ec8258f1c713e28f9ed4826ffdff8d
CShinoDev <davidqo197@gmail.com>
2025-09-07 13:47:32 -0500

    Actualizacion

--- vendoor/vendoor/urls.py
+++ vendoor/vendoor/urls.py
@@ -4,11 +4,12 @@ from django.views.generic import TemplateView
 from vendoor.views import test_index 
 
 urlpatterns = [
+     # Servir React para todas las rutas no capturadas por la API o admin
+    path('api/', test_index, name='home'),
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/', include('productos.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
-    # Servir React para todas las rutas no capturadas por la API o admin
-    path('', test_index, name='home'),
+   
 ]
